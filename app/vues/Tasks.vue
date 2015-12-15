4917809e59ee813367c1d4bedd95621c3c3d98e7
Adrien Leloup <contact@void.graphics>
2015-12-15 17:22:58 +0100

    Add delete popup

--- app/vues/Tasks.vue
+++ app/vues/Tasks.vue
@@ -1,16 +1,21 @@
 <template src="../html/projecttasks.html"></template>
 
 <script lang="coffee">
-
-
     Dragula = require "dragula"
     Tasks =
+        items: []
         data: ->
-            return { tasks: [ { title: "not loaded yet" } ] }
+            return {
+                popupIsShowing: false
+                taskToDelete: null
+                tasks: [ { title: "not loaded yet" } ]
+             }
 
         asyncData: ( resolve, reject ) ->
-            socket.emit "task.getAll", ( aReturnedTasks ) ->
-                resolve { tasks: aReturnedTasks }
+            socket.emit "task.getAll", ( oReturnedTasks ) ->
+                this.items = Object.keys( oReturnedTasks ).map( ( key ) -> return oReturnedTasks[ key ] )
+                resolve { tasks: this.items }
+
         ready: ->
             drake = Dragula( [ document.querySelector( '#todo-column__items' ), document.querySelector( '#progress-column__items' ), document.querySelector( '#finished-column__items' ) ] )
             that = this
@@ -28,6 +33,34 @@
                 column = event.target.parentNode.id
                 console.log column
 
+            editPopup: ( event ) ->
+                console.log 'editing', event.target.parentNode
+
+            showDeletePopup: ( event ) ->
+                id = parseInt event.target.parentNode.dataset.id
+                this.taskToDelete = id
+                this.popupIsShowing = true
+
+            delete: ->
+                # Deleting client-side
+                id = this.taskToDelete
+                taskToDelete = null
+                for task in this.tasks
+                    taskToDelete = task if task.id == id
+                index = this.tasks.indexOf( taskToDelete )
+                this.tasks.splice( index, 1 )
+
+                # Deleting server-side
+                socket.emit "task.delete", id
+
+                this.popupIsShowing = false
+
+        events:
+            hidePopup: ->
+                this.popupIsShowing = false
+
+            confirmDelete: ->
+                this.delete()
 
     module.exports = Tasks
 
