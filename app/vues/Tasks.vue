61f05e58bb98f81482dc38b188ce320fe89ad7fc
Adrien Leloup <contact@void.graphics>
2015-12-15 17:22:40 +0100

    Add Dragula

--- app/vues/Tasks.vue
+++ app/vues/Tasks.vue
@@ -3,6 +3,7 @@
 <script lang="coffee">
 
 
+    Dragula = require "dragula"
     Tasks =
         data: ->
             return { tasks: [ { title: "not loaded yet" } ] }
@@ -10,6 +11,17 @@
         asyncData: ( resolve, reject ) ->
             socket.emit "task.getAll", ( aReturnedTasks ) ->
                 resolve { tasks: aReturnedTasks }
+        ready: ->
+            drake = Dragula( [ document.querySelector( '#todo-column__items' ), document.querySelector( '#progress-column__items' ), document.querySelector( '#finished-column__items' ) ] )
+            that = this
+            drake.on "dragend", ( element ) ->
+                sNewState = element.parentNode.parentNode.id
+                index = element.dataset.id
+                taskToUpdate = null
+                for task in that.tasks
+                    taskToUpdate = task if task.id == index
+                that.tasks[index].state = sNewState
+                socket.emit "task.changeState", index, sNewState
 
         methods:
             addPopup: ( event ) ->
