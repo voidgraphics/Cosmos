6b955843f6ad4fb6355bbd57e7e7a772cda2a889
Adrien Leloup <contact@void.graphics>
2016-01-19 04:33:20 +0100

    Change server addr

--- app/js/socket.js
+++ app/js/socket.js
@@ -2,7 +2,7 @@
 
 localStorage.removeItem( "debug" );
 
-var socket = io( "http://localhost:12345" );
+var socket = io( "http://www.intothevoid.space:12346" );
 
 socket.on( "connect", function () {
      console.log( "Socket successfully connected." );
