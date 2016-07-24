3fb107ae55dc152be395b6e3dd4954a7e09f90b4
Adrien Leloup <contact@void.graphics>
2016-07-24 22:45:34 +0200

    Added user account creation

--- app/vues/SignUp.vue
+++ app/vues/SignUp.vue
@@ -1,3 +1,33 @@
-<template src="../html/signup.html">
+<template src="../html/signup.html"></template>
 
-</template>
+<script lang="coffee">
+
+    SignUp =
+        data: ->
+            return {
+                username: ""
+                email: ""
+                firstname: ""
+                lastname: ""
+                password: ""
+            }
+
+        methods:
+            register: ->
+                if @username && @email && @password
+                    console.log "Registering #{@username}..."
+                    oUserInfo =
+                        username: @username
+                        email: @email
+                        firstname: @firstname
+                        lastname: @lastname
+                        password: @password
+
+                    socket.emit "user.register", oUserInfo, ( oResult ) ->
+                        console.log oResult
+                        
+                else
+                    console.error "Missing info"
+
+    module.exports = SignUp
+</script>
