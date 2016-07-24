62c78e9e9672e7b5ea6c2dbbd05a50af44bfb390
Adrien Leloup <contact@void.graphics>
2016-07-24 22:43:42 +0200

    Added sign in functionality

--- app/vues/SignIn.vue
+++ app/vues/SignIn.vue
@@ -1,7 +1,39 @@
-<template src="../html/connect.html">
+<template src="../html/connect.html"></template>
 
-</template>
+<script lang="coffee">
 
-<script type="text/javascript">
+    SignIn =
+        data: ->
+            return {
+                username: ""
+                password: ""
+                usernameError: ""
+                passwordError: ""
+            }
 
+        ready: ->
+            that = this
+            socket.on "user.logged", ( oUserData ) ->
+                localStorage.id = oUserData.id
+                localStorage.username = oUserData.username
+                localStorage.firstname = oUserData.firstname
+                localStorage.lastname = oUserData.lastname
+
+                that.$route.router.go "/tasks"
+
+        methods:
+            submit: ->
+                if @username && @password
+                    socket.emit "user.login", { username: @username, password: @password }
+                else
+                    if !@username
+                        @usernameError = "Please enter your username"
+                    else
+                        @usernameError = ""
+                    if !@password
+                        @passwordError = "Please enter your password"
+                    else
+                        @passwordError = ""
+
+    module.exports = SignIn
 </script>
