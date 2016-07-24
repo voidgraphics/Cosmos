47deb911f2d7ac0e66462fb25e09f864a502dd1b
Adrien Leloup <contact@void.graphics>
2016-07-24 22:45:00 +0200

    Fetch mockups from server

--- app/vues/Mockups.vue
+++ app/vues/Mockups.vue
@@ -6,72 +6,22 @@
         items: []
         data: ->
             return {
-                popupIsShowing: false
-                deletePopupIsShowing: false
-                taskToDelete: null
-                columnName: null
-                tasks: [ { title: "not loaded yet" } ]
+                mockups: []
              }
 
-        asyncData: ( resolve, reject ) ->
-            socket.emit "mockup.getAll", ( oReturnedMockups ) ->
-                this.items = Object.keys( oReturnedMockups ).map( ( key ) -> return oReturnedMockups[ key ] )
-                console.log oReturnedMockups
-                resolve { tasks: this.items }
+        # asyncData: ( resolve, reject ) ->
+            # socket.emit "mockup.getAll", ( oReturnedMockups ) ->
+            #     this.items = Object.keys( oReturnedMockups ).map( ( key ) -> return oReturnedMockups[ key ] )
+            #     resolve { tasks: this.items }
 
         ready: ->
+            socket.emit "mockup.getAll"
+            that = this
+            socket.on "mockup.sent", ( oMockup ) ->
+                console.log oMockup
+                oMockup.src = "data:image/png;base64,#{oMockup.image}"
+                that.mockups.push oMockup
 
-        methods:
-            addPopup: ( event ) ->
-                column = event.target.parentNode.id
-                console.log column
-
-            editPopup: ( event ) ->
-                console.log 'editing', event.target.parentNode
-
-            showPopup: ( event )  ->
-                this.columnName = event.target.parentNode.id
-                this.popupIsShowing = true
-
-            showEditPopup: ( oTask ) ->
-                this.taskToEdit = oTask
-                console.log "Editing:", oTask
-                this.popupIsShowing = true
-
-            showDeletePopup: ( task ) ->
-                this.taskToDelete = task
-                this.deletePopupIsShowing = true
-
-            delete: ->
-                # Deleting client-side
-                this.tasks.$remove( this.taskToDelete )
-
-                # Deleting server-side
-                socket.emit "task.delete", this.taskToDelete.id
-
-                this.deletePopupIsShowing = false
-
-        events:
-            hidePopup: ->
-                this.popupIsShowing = false
-
-            hideDeletePopup: ->
-                this.deletePopupIsShowing = false
-
-            confirmDelete: ->
-                this.delete()
-
-            submitTask: ( oTask ) ->
-                console.log oTask
-                oTask.id = zouti.uuid()
-                socket.emit "task.save", oTask
-                this.tasks.push( oTask )
-                for task in this.tasks
-                    if task.state == oTask.state && task.id != oTask.id
-                        task.position++
-                        console.log task.title, task.position
-
-                this.popupIsShowing = false
 
     module.exports = Tasks
 
