e8266edd1f98157f898e8f5576fdf33f4a950bd6
Adrien Leloup <contact@void.graphics>
2016-08-15 20:52:18 +0200

    Added new mockup logic

--- app/vues/Mockups.vue
+++ app/vues/Mockups.vue
@@ -7,15 +7,47 @@
         data: ->
             return {
                 mockups: []
+                popupIsShowing: false
+
+                newMockup: {
+                    name: ""
+                    file: null
+                }
              }
 
         ready: ->
-            socket.emit "mockup.getAll"
-            socket.on "mockup.sent", ( oMockup ) =>
+            socket.emit "mockup.getAll", localStorage.selectedProject
+            socket.on "mockup.sent", ( oMockup, iCommentCount ) =>
                 oMockup.src = "data:image/png;base64,#{oMockup.image}"
                 oMockup.href = "/mockups/#{oMockup.id}"
+                oMockup.commentCount = iCommentCount
                 @mockups.push oMockup
 
+        methods:
+            togglePopup: ->
+                @popupIsShowing = !@popupIsShowing
+
+            addMockup: ->
+                oMockup = {
+                    name: @newMockup.name
+                    file: @newMockup.file
+                    image: @newMockup.fileName
+                    projectId: localStorage.selectedProject
+                }
+                socket.emit "mockup.create", oMockup
+
+            detectFile: ( e ) ->
+                @newMockup.fileName = e.target.files[0].name
+                reader = new FileReader()
+                reader.onload = ( e ) =>
+                    @newMockup.file = e.target.result
+                reader.readAsDataURL e.target.files[0]
+
+        events:
+            changeProject: ( oTeam, oProject ) ->
+                console.log @mockups
+                @mockups = []
+                socket.emit "mockup.getAll", oProject.uuid
 
     module.exports = Tasks
 
